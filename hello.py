df9c8b14d42b2a4b6498ba8ba6597957c1d5e52f
Pujitha Lakshmi Perisetla <pujitha.perisetla@solugenix.com>
2025-09-11 12:28:25 +0530

    flask APIs added

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
-print("Hello world")
+from flask import Flask, jsonify
 
-print("How are you")
-print("to check jenkins")
+app = Flask(__name__)
+
+@app.route("/hello", methods=["GET"])
+def hello_world():
+    return jsonify({"message": "Hello World"})
+
+@app.route("/status", methods=["GET"])
+def status():
+    return jsonify({"message": "Running on /status"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
